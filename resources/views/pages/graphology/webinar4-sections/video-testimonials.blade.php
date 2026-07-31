6f8971405bd5dae8eab6df0ba161dfece104d0df
Ajitsinghoccultscience <ajitsingh.occultscience@gmail.com>
2026-07-31 10:39:01 +0530

    dates

--- resources/views/pages/graphology/webinar4-sections/video-testimonials.blade.php
+++ resources/views/pages/graphology/webinar4-sections/video-testimonials.blade.php
@@ -1,11 +1,11 @@
 @php
     $videos = [
         ['name' => 'Sahil parate', 'date' => '18-02-2026', 'video' => '8_cNdDif9ys'],
-        ['name' => 'Kunj Bihari Sharma', 'date' => '18-02-2026', 'video' => 'RI637QVE648'],
-        ['name' => 'Devanshu Sharma', 'date' => '18-02-2026', 'video' => '9JlMu72MWhE'],
-        ['name' => 'Suraj Kumar', 'date' => '18-02-2026', 'video' => 'GW4WpHXgb_4'],
-        ['name' => 'Manjeet Kumar', 'date' => '18-02-2026', 'video' => 'mEcnaSkIVfY'],
-        ['name' => 'Jigyasu Tiwari', 'date' => '18-02-2026', 'video' => 'mdykrN_t0zs'],
+        ['name' => 'Kunj Bihari Sharma', 'date' => '21-02-2026', 'video' => 'RI637QVE648'],
+        ['name' => 'Devanshu Sharma', 'date' => '25-02-2026', 'video' => '9JlMu72MWhE'],
+        ['name' => 'Suraj Kumar', 'date' => '02-03-2026', 'video' => 'GW4WpHXgb_4'],
+        ['name' => 'Manjeet Kumar', 'date' => '07-03-2026', 'video' => 'mEcnaSkIVfY'],
+        ['name' => 'Jigyasu Tiwari', 'date' => '12-03-2026', 'video' => 'mdykrN_t0zs'],
     ];
 @endphp
 
